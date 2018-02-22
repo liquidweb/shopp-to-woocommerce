b3875365c40b90d694539082962d16caa2c4a61d
Steve Grunwell <sgrunwell@liquidweb.com>
2018-02-22 22:14:23 +0000

    Don't worry about running Behat in Travis, since we're not using it

--- bin/test.sh
+++ bin/test.sh
@@ -9,5 +9,5 @@ then
 fi
 
 # Run the functional tests
-BEHAT_TAGS=$(php utils/behat-tags.php)
-behat --format progress $BEHAT_TAGS --strict
+#BEHAT_TAGS=$(php utils/behat-tags.php)
+#behat --format progress $BEHAT_TAGS --strict
